12c814aaf55f206bac95acd156da49b3f96ca63c
Raymond Mapuranga <veliko.outro@gmail.com>
2017-06-04 15:53:58 -0400

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
+# Create your views here.
+def signup(request):
+	if request.method == 'POST':
+		if request.POST['password1'] == request.POST['password2']:
+			try:
+				user = User.objects.get(username=request.POST['username'])
+				return render(request, 'accounts/signup.html',{'error':'Username has already been taken'})
+			except User.DoesNotExist:
+				user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+				login(request, user)
+				return render(request, 'accounts/signup.html')
+		else:
+			return render(request, 'accounts/signup.html',{'error':'Passwords didn\'t match\n\n'})
+	else:
+		return render(request, 'accounts/signup.html')
+
+
+def loginview(request):
+	if request.method == 'POST':
+		user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+		if user is not None:
+			login(request, user)
+			return render(request, 'accounts/login.html',{'error':'Login successful!'})
+		else:
+			return render(request, 'accounts/login.html',{'error':'The Username and Password didn\'t match\n\n'})
+	else:
+		return render(request, 'accounts/login.html')
+
+
+
---
+++ posts/views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def create(request):
+	return render(request, 'posts/create.html',{})
